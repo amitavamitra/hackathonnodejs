7e4d94843cff03e314737cd6f8cc912447e817ce
amitavamitra <amitava.mitra@hotmail.co.uk>
2021-05-06 12:07:45 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const  express = require('express');
+const ejs = require('ejs')
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine' , 'ejs');
+
+app.get('/', function(req,res){
+    res.render('home');
+})
+
+app.listen(3000, function(){
+    console.log('Azure DevOps NodeJs app running at port 3000');
+})
